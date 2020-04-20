f153471ef7d2161a3a22b0399e419fee6e47f2ea
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-20 10:49:09 -0600

    Fix formatting

--- app/models/trip_traveler.rb
+++ app/models/trip_traveler.rb
@@ -1,4 +1,5 @@
 class TripTraveler < ApplicationRecord
   belongs_to :trip
   belongs_to :traveler
+
 end
--- spec/models/trip_traveler_spec.rb
+++ spec/models/trip_traveler_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe TripTraveler do
-  describe 'relationships' do
+  describe 'Relationships' do
     it {should belong_to :trip}
     it {should belong_to :traveler}
   end
+
 end
