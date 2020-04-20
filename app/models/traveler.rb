aaabf8fdc85742f3ed01682518df1997972fb2d0
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-20 09:22:28 -0600

    Add belongs to and has many to model

---
+++ app/models/traveler.rb
@@ -0,0 +1,6 @@
+class Traveler < ApplicationRecord
+  has_many :traveler_trips
+  has_many :trips, through: :traveler_trips
+
+  validates_presence_of :name, :age
+end
---
+++ app/models/trip.rb
@@ -0,0 +1,6 @@
+class Trip < ApplicationRecord
+  has_many :traveler_trips
+  has_many :travelers, through: :traveler_trips
+
+  validates_presence_of :title, :destination_city, :mileage
+end
