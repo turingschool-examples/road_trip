51d51f39000b51df4352119ed02d3672718a57c8
Ryan Camp <campryan@comcast.net>
2020-04-20 09:56:18 -0600

    Write test for .sort_by_mileage(asc/desc) for Trip

--- spec/model/trip_spec.rb
+++ spec/model/trip_spec.rb
@@ -16,5 +16,18 @@ RSpec.describe Trip, type: :model do
   describe "Instance Methods" do
 
   end
-
+  describe "Class Methods" do
+    before(:each) do
+      @trip1 = Trip.create!(title: "Cheese Tour 2020", destination_city: "Madison, WI", mileage: 1100)
+      @trip2 = Trip.create!(title: "Who is America Anyway?", destination_city: "Washington, D.C.", mileage: 300)
+      @trip3 = Trip.create!(title: "The Big Apple", destination_city: "New York City, NY", mileage: 850)
+      @trip4 = Trip.create!(title: "Bike n’ Climb", destination_city: "Moab, UT", mileage: 700)
+    end
+    it ".sort_by_mileage" do
+      expect(Trip.sort_by_mileage(:asc)[0]).to eql(@trip2)
+      expect(Trip.sort_by_mileage(:asc)[1]).to eql(@trip4)
+      expect(Trip.sort_by_mileage(:asc)[2]).to eql(@trip3)
+      expect(Trip.sort_by_mileage(:asc)[3]).to eql(@trip1)
+    end
+  end
 end
