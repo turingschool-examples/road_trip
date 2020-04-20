133d83795cab6e050b50ebf296eb370979e32352
Kevin McGrevey <niv3kmcg@gmail.com>
2020-04-20 10:50:03 -0600

    Adds test to delete traveler

---
+++ spec/features/traveler_delete_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe "When I visit a trips show page as a visitor", type: :feature do
+  before(:each) do
+    @trip1 = Trip.create!(title: "Cheese Tour 2020",
+                          dest_city: "Madison, WI",
+                          milage: 1100)
+    @trip2 = Trip.create!(title: "Who is America Anyway?",
+                        dest_city: "Washington, DC",
+                        milage: 300)
+    @trip3 = Trip.create!(title: "The Big Apple",
+                        dest_city: "New York City, NY",
+                        milage: 850)
+
+    @traveler1 = Traveler.create!(name: "Sally Sue", age: 25)
+    @traveler2 = Traveler.create!(name: "Tommy Tom", age: 46)
+    @traveler3 = Traveler.create!(name: "Bob", age: 50)
+
+    TripTraveler.create(trip_id: @trip1.id, traveler_id: @traveler1.id)
+    TripTraveler.create(trip_id: @trip1.id, traveler_id: @traveler2.id)
+    
+    TripTraveler.create(trip_id: @trip2.id, traveler_id: @traveler1.id)
+    TripTraveler.create(trip_id: @trip2.id, traveler_id: @traveler3.id)
+  end
+
+  it "I see click button next to a travelers name to remove them from trip" do
+    visit "/trips/#{@trip1.id}"
+
+    expect(page).to have_content(@traveler1.name)
+    expect(page).to have_content(@traveler2.name)
+
+    within "#traveler-#{@traveler1.id}" do
+      click_button "Remove Traveler"
+    end
+
+    expect(page).not_to have_content(@traveler1.name)
+    expect(page).to have_content(@traveler2.name)
+  
+  end
+
+end
+
+
+#   As a visitor
+# When I visit a trips show page
+# Next to each traveler’s name
+# I see a button to remove that traveler from the trip
+# When I click that button for a particular traveler
+# I am redirected back to the trips show page
+# And I no longer see that traveler’s name listed
