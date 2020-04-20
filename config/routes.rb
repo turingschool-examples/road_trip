12ecefcbbb0efac3ef7d787ebb50a52bd29e32a1
Anthony Gomes <code@tonygom.es>
2020-04-20 11:29:40 -0600

    Add TripTraveler destroy route to config

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :trips, only: [:index, :show]
+
+  delete '/trip_travelers/:trip_id/:traveler_id', to: 'trip_travelers#destroy'
 end
