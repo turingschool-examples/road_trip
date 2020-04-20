bb02f0782e6788f4ac326567e16f46d2bbcbe732
Fred Rondina <fredrondina96@gmail.com>
2020-04-20 11:55:11 -0600

    Cleaned up test files

--- spec/features/trips/index_spec.rb
+++ spec/features/trips/index_spec.rb
@@ -15,13 +15,13 @@ RSpec.describe "As a user, when I visit the trip index page", type: :feature do
   end
 
   it "I see all trips listed in order of their mileage ascending" do
-    #will come back and properly test with orderly if I have time
+
     @trip1 = Trip.create!(title: "Go to Utah", destination: "Moab, UT", miles: 2000)
     @trip2 = Trip.create!(title: "Cheese Tour 2020", destination: "Madison, WI", miles: 1100)
     @trip3 = Trip.create!(title: "Visit Key West", destination: "Key West, FL", miles: 2900)
 
     visit "/trips"
-  
+    #may come back to test with orderly if there is time
     expect(page).to have_content(@trip1.title)
     expect(page).to have_content(@trip2.title)
     expect(page).to have_content(@trip3.title)
