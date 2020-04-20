9b367fbefffdde80a8e71f72ab1a53b2095c1e98
Ryan Camp <campryan@comcast.net>
2020-04-20 09:06:04 -0600

    Initial setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Trip1
+# title: “Cheese Tour 2020”
+# destination_city: “Madison, WI”
+# mileage: 1100
+#
+# Trip2
+# title: “Who is America Anyway?”
+# destination_city: “Washington, D.C.”
+# mileage: 300
+#
+# Trip3
+# title: “The Big Apple”
+# destination_city: “New York City, NY”
+# mileage: 850
+#
+# Trip4
+# title: “Bike n’ Climb”
+# destination_city: “Moab, UT”
+# mileage: 700
+#
+# Traveler1
+# name: “Sally Sue”
+# age: 25
+#
+# Traveler2
+# name: “Tommy Tom”
+# age: 46
+#
+# Traveler3
+# name: “Cory Cory”
+# age: 19
+#
+# Traveler4
+# name: “Mary Mae”
+# age: 44
+#
+# Traveler5
+# name: “Smith John”
+# age: 9
