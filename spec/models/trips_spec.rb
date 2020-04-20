8bd753f72a27da18bf094b898999ce4f375de38c
Ethan Hocking <eshocking253@gmail.com>
2020-04-20 10:39:24 -0600

    Add spec for trips model

---
+++ spec/models/trips_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Trip, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :destination_city }
+    it { should validate_presence_of :mileage }
+  end
+
+  describe "relationships" do
+    it { should have_many :traveler_trips }
+    it { should have_many(:travelers).through(:traveler_trips) }
+  end
+end
