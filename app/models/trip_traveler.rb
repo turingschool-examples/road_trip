00bc8cb9e6cc6c3ef0e99d23b57e9f0a45165e5a
Maxwell Baird <bairdm71@gmail.com>
2020-04-20 11:22:14 -0600

    added validations to triptraveler

--- app/models/trip_traveler.rb
+++ app/models/trip_traveler.rb
@@ -1,4 +1,5 @@
 class TripTraveler < ApplicationRecord
+  validates_presence_of :trip_id, :traveler_id
   belongs_to :traveler
   belongs_to :trip
 end
--- spec/models/trip_traveler_spec.rb
+++ spec/models/trip_traveler_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TripTraveler, type: :model do
+
+  describe "validations" do
+    it { should validate_presence_of :traveler_id }
+    it { should validate_presence_of :trip_id }
+  end
+
   describe "relationships" do
     it {should belong_to :trip}
     it {should belong_to :traveler}
