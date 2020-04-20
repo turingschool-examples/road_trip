07a4e319aae46980015855716dae69d8040a04f2
Maxwell Baird <bairdm71@gmail.com>
2020-04-20 11:10:54 -0600

    created the model test for some methods in trips

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,8 +1,42 @@
 require "rails_helper"
 
 RSpec.describe Trip, type: :model do
+
   describe "relationships" do
     it { should have_many :trip_travelers}
     it { should have_many(:travelers).through(:trip_travelers)}
+  end
+
+  describe 'methods' do
+    it 'similar_trips' do
+      trip1 = Trip.create(title: "Bike n’ Climb",
+                          destination_city: "Moab, UT",
+                          mileage: 700)
+
+      trip2 = Trip.create(title: "Scary Houses",
+                          destination_city: "Madison, WI",
+                          mileage: 2000)
+
+      trip3 = Trip.create(title: "Cheese and Wine",
+                          destination_city: "Madison, WI",
+                          mileage: 1100)
+      expect(trip2.similar_trips).to eq([trip3])
+      expect(trip1.similar_trips).to eq([])
+    end
+
+    it 'by_mileage' do
+      trip1 = Trip.create(title: "Bike n’ Climb",
+                          destination_city: "Moab, UT",
+                          mileage: 700)
+
+      trip2 = Trip.create(title: "Scary Houses",
+                          destination_city: "Madison, WI",
+                          mileage: 2000)
+
+      trip3 = Trip.create(title: "Cheese and Wine",
+                          destination_city: "Madison, WI",
+                          mileage: 1100)
+      expect(Trip.by_mileage).to eq([trip1, trip3, trip2])
+    end
   end
 end
