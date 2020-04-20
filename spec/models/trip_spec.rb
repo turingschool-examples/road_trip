d3f575721aa0e35dc1b88cf3d5edd22342ec349c
Colin Alexander <colintalex@gmail.com>
2020-04-20 11:43:32 -0600

    Updates before each to run properly in describe statement

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -5,18 +5,20 @@ RSpec.describe Trip, type: :model do
     it { should have_many(:travelers).through(:traveler_trips)}
   end
 
-  before(:each) do
-    @traveler_1 = Traveler.create(name: 'Sally Sue', age: 25)
-    @trip_1 = @traveler_1.trips.create(title: "Cheese Tour 2020", destination: "Madison, WI", mileage: 900)
-    @trip_2 = @traveler_1.trips.create(title: "Walk America", destination: "Santa Fe, NM", mileage: 5100)
-    @trip_3 = @traveler_1.trips.create(title: "National Bar Crawl", destination: "Madison, WI", mileage: 1100)
-    @trip_4 = @traveler_1.trips.create(title: "Scooter Tour", destination: "Green Bay, WI", mileage: 40)
-
-    @traveler_2 = @trip_1.travelers.create(name: 'Dapper Dan', age: 22)
-    @traveler_3 = @trip_1.travelers.create(name: 'Timmy Tom', age: 29)
-  end
-
   describe 'Instance Methods' do
-    expect(@trip_1.similar_trips).to eql(@trip_3)
+    before(:each) do
+      @traveler_1 = Traveler.create(name: 'Sally Sue', age: 25)
+      @trip_1 = @traveler_1.trips.create(title: "Cheese Tour 2020", destination: "Madison, WI", mileage: 900)
+      @trip_2 = @traveler_1.trips.create(title: "Walk America", destination: "Santa Fe, NM", mileage: 5100)
+      @trip_3 = @traveler_1.trips.create(title: "National Bar Crawl", destination: "Madison, WI", mileage: 1100)
+      @trip_4 = @traveler_1.trips.create(title: "Scooter Tour", destination: "Green Bay, WI", mileage: 40)
+
+      @traveler_2 = @trip_1.travelers.create(name: 'Dapper Dan', age: 22)
+      @traveler_3 = @trip_1.travelers.create(name: 'Timmy Tom', age: 29)
+    end
+
+    it ".similar_trips()" do
+      expect(@trip_1.similar_trips.to_a).to eql([@trip_3])
+    end
   end
 end
