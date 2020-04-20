a79065cf310bbdf87ecb6cae743ac8aa77a4b0eb
Ryan Camp <campryan@comcast.net>
2020-04-20 09:38:06 -0600

    Create seeds from teacher supplied info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,46 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# Trip1
-# title: “Cheese Tour 2020”
-# destination_city: “Madison, WI”
-# mileage: 1100
-#
-# Trip2
-# title: “Who is America Anyway?”
-# destination_city: “Washington, D.C.”
-# mileage: 300
-#
-# Trip3
-# title: “The Big Apple”
-# destination_city: “New York City, NY”
-# mileage: 850
-#
-# Trip4
-# title: “Bike n’ Climb”
-# destination_city: “Moab, UT”
-# mileage: 700
-#
-# Traveler1
-# name: “Sally Sue”
-# age: 25
-#
-# Traveler2
-# name: “Tommy Tom”
-# age: 46
-#
-# Traveler3
-# name: “Cory Cory”
-# age: 19
-#
-# Traveler4
-# name: “Mary Mae”
-# age: 44
-#
-# Traveler5
-# name: “Smith John”
-# age: 9
+#SEEDS:
+#Trip
+Trip1 = Trip.create!(title: "Cheese Tour 2020", destination_city: "Madison, WI", mileage: 1100)
+Trip2 = Trip.create!(title: "Who is America Anyway?", destination_city: "Washington, D.C.", mileage: 300)
+Trip3 = Trip.create!(title: "The Big Apple", destination_city: "New York City, NY", mileage: 850)
+Trip4 = Trip.create!(title: "Bike n’ Climb", destination_city: "Moab, UT", mileage: 700)
+#Traveler
+Traveler1 = Traveler.create!(name: "Sally Sue", age: 25)
+Traveler2 = Traveler.create!(name: "Tommy Tom", age: 46)
+Traveler3 = Traveler.create!(name: "Cory Cory", age: 19)
+Traveler4 = Traveler.create!(name: "Mary Mae", age: 44)
+Traveler5 = Traveler.create!(name: "Smith John", age: 9)
