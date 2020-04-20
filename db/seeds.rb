a28dccfe7241a70b636d71cb66bc0418093d5192
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-04-20 10:59:39 -0600

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+trip1 = Trip.create!(title: "Cheese Tour 2020", city: "Madison, WI", mileage: 1100)
+trip2 = Trip.create!(title: "Who is America Anyway?", city: "Washington, D.C.", mileage: 300)
+trip3 = Trip.create!(title: "The Big Apple", city: "New York City, NY", mileage: 850)
+trip4 = Trip.create!(title: "Bike n’ Climb", city: "Moab, UT", mileage: 700)
+trip5 = Trip.create!(title: "Arches", city: "Moab, UT", mileage: 900)
+
+traveler1 = Traveler.create!(name: "Sally Sue", age: 25)
+traveler2 = Traveler.create!(name: "Tommy Tom", age: 46)
+traveler3 = Traveler.create!(name: "Cory Cory", age: 19)
+traveler4 = Traveler.create!(name: "Mary Mae", age: 44)
+traveler5 = Traveler.create!(name: "Smith John", age: 9)
+
+TravelerTrip.create!(trip_id: trip1.id, traveler_id: traveler1.id)
+TravelerTrip.create!(trip_id: trip1.id, traveler_id: traveler2.id)
+TravelerTrip.create!(trip_id: trip1.id, traveler_id: traveler3.id)
+
+TravelerTrip.create!(trip_id: trip2.id, traveler_id: traveler2.id)
+TravelerTrip.create!(trip_id: trip2.id, traveler_id: traveler3.id)
+
+TravelerTrip.create!(trip_id: trip3.id, traveler_id: traveler1.id)
+TravelerTrip.create!(trip_id: trip3.id, traveler_id: traveler3.id)
+TravelerTrip.create!(trip_id: trip3.id, traveler_id: traveler4.id)
+TravelerTrip.create!(trip_id: trip3.id, traveler_id: traveler5.id)
+
+TravelerTrip.create!(trip_id: trip4.id, traveler_id: traveler2.id)
+TravelerTrip.create!(trip_id: trip4.id, traveler_id: traveler4.id)
+TravelerTrip.create!(trip_id: trip4.id, traveler_id: traveler5.id)
