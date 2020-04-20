d8248b1d4a2da2f93a6e8974e1770fb8955ae1ec
Margo Flewelling <flewelling.margo@gmail.com>
2020-04-20 10:10:19 -0600

    Add edit action for removing a traveler

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -8,4 +8,12 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
   end
 
+  def edit
+    trip = Trip.find(params[:trip_id])
+    traveler = Traveler.find(params[:traveler_id])
+    trip.travelers.delete(traveler)
+    redirect_to "/trips/#{trip.id}"
+  end
+
+
 end
