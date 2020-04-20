9da7f4b3cc66a134f7c3612c83d1c03d702933f4
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-20 09:21:38 -0600

    Wrote tests for relationships

---
+++ spec/models/traveler_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Traveler, type: :model do
+  describe 'relationships' do
+    it {should have_many :traveler_trips}
+    it {should have_many(:trips).through(:traveler_trips)}
+
+  end
+end
---
+++ spec/models/trip_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Trip, type: :model do
+  describe 'relationships' do
+    it {should have_many :traveler_trips}
+    it {should have_many(:travelers).through(:traveler_trips)}
+
+  end
+end
