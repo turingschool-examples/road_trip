3ebc9a06b2be776cefe0b0f54902a80b6e4230f6
David Tran <davidttemail@gmail.com>
2020-04-20 10:19:03 -0600

    Write test for displaying trips with similar destination

--- spec/features/trips/show_spec.rb
+++ spec/features/trips/show_spec.rb
@@ -4,6 +4,7 @@ describe "trips show page" do
   before(:each) do
     @trip_1 = Trip.create!(title: "Cheese Tour 2020", destination_city: "Madison, WI", mileage: 1100)
     @trip_2 = Trip.create!(title: "Who is America Anyway?", destination_city: "Washington, D.C.", mileage: 300)
+    @trip_3 = Trip.create!(title: "Capital Ahoy", destination_city: "Washington, D.C.", mileage: 400)
     @traveler_1 = Traveler.create(name: "Sally Sue", age: 25)
     @traveler_2 = Traveler.create(name: "Tommy Tom", age: 46)
     @traveler_3 = Traveler.create(name: "Cory Cory", age: 19)
@@ -38,6 +39,16 @@ describe "trips show page" do
     within("#travelers-list") do
       expect(page).to have_no_content(@traveler_1.name)
       expect(page).to have_content(@traveler_2.name)
+    end
+  end
+
+  it "sees other trip titles to the same destination" do
+    visit "/trips/#{@trip_2.id}"
+
+    expect(page).to have_content("Other Trips to this Destination")
+    within("#similar-trips") do
+      expect(page).to have_link(@trip_3.title)
+      expect(page).to have_no_link(@trip_2.title)
     end
   end
 end
