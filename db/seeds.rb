745c59c3592bd954dbd3c61a27fe01487f48061f
Javier Aguilar <jvr.agx@gmail.com>
2020-04-20 10:13:23 -0600

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Trips
+trip1 = Trip.create(title: "Cheese Tour 2020", destination: "Madison, WI", mileage: 1100)
+trip2 = Trip.create(title: "“Who is America Anyway?", destination: "Washington, D.C.", mileage: 300)
+trip3 = Trip.create(title: "The Big Apple", destination: "Moab, UT", mileage: 700)
+trip4 = Trip.create(title: "Bike n’ Climb", destination: "New York City, NY", mileage: 850)
+
+# Travelers
+traveler1 = Traveler.create(name: "Sally Sue", age: 25)
+traveler2 = Traveler.create(name: "Tommy Tom", age: 46)
+traveler3 = Traveler.create(name: "Cory Cory", age: 19)
+traveler4 = Traveler.create(name: "Mary Mae", age: 44)
+traveler5 = Traveler.create(name: "Smith John", age: 9)
+
+# Add Travelers to Trips
+trip1.travelers << [traveler1, traveler2]
+trip2.travelers << [traveler1, traveler3]
+trip4.travelers << [traveler4]
