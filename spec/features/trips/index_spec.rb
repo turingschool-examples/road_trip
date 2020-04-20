16de93685b235fc5547849364293a4cfd4ad9103
Tyler Porter <tylerpporter@gmail.com>
2020-04-20 09:36:50 -0600

    Complete User Story 1

---
+++ spec/features/trips/index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  before :each do
+    @trip1 = Trip.create!(title: "Fun", destination: "Madison", mileage: 300)
+    @trip2 = Trip.create!(title: "Exciting", destination: "Nashville", mileage: 200)
+    @trip3 = Trip.create!(title: "OK", destination: "Denver", mileage: 100)
+  end
+describe 'When I visit /trips'
+  it 'I see the titles of all trips in my database listed in order of their mileage ' do
+    visit '/trips'
+
+    expect(page).to have_content(@trip1.title)
+    expect(page).to have_content(@trip2.title)
+    expect(page).to have_content(@trip3.title)
+
+    @trip3.title.should appear_before(@trip2.title)
+    @trip3.title.should appear_before(@trip1.title)
+    @trip2.title.should appear_before(@trip1.title)
+  end
+
+end
