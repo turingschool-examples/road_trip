2e7409d6fccbfa1542cf2739166cb7249b9fe087
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-20 11:11:44 -0600

    Add test for similar trips model method

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -13,4 +13,23 @@ RSpec.describe Trip, type: :model do
     it {should validate_presence_of :mileage}
   end
 
+  describe "Instance Methods" do
+    before :each do
+      @trip_1 = Trip.create!(title: "Cheese Tour 2020", destination_city: "Madison, WI", mileage: 1100)
+      @trip_2 = Trip.create!(title: "That 70's Tour", destination_city: "Madison, WI", mileage: 300)
+      @trip_3 = Trip.create!(title: "Great Lakes Vista", destination_city: "Madison, WI", mileage: 800)
+      @trip_4 = Trip.create!(title: "Frigid Heights", destination_city: "Madison, WI", mileage: 750)
+
+      @trip_5 = Trip.create!(title: "Who is America Anyway?", destination_city: "Washington, D.C.", mileage: 300)
+      @trip_6 = Trip.create!(title: "The Big Apple", destination_city: "New York City, NY", mileage: 850)
+      @trip_7 = Trip.create!(title: "Bike n’ Climb", destination_city: "Moab, UT", mileage: 700)
+    end
+
+    it ".similar_trips" do
+      similar_trips = [@trip_2, @trip_3, @trip_4]
+
+      expect(@trip_1.similar_trips).to eq(similar_trips)
+    end
+  end
+
 end
