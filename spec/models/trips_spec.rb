6cf0ab64d1db46d4e92a4e56e8cfdbfbee79ca15
Ethan Hocking <eshocking253@gmail.com>
2020-04-20 11:04:25 -0600

    Add class method spec to trips spec

--- spec/models/trips_spec.rb
+++ spec/models/trips_spec.rb
@@ -11,4 +11,30 @@ RSpec.describe Trip, type: :model do
     it { should have_many :traveler_trips }
     it { should have_many(:travelers).through(:traveler_trips) }
   end
+
+  context "methods" do
+    before :each do
+      @trip1 = Trip.create(title: "Cheese Tour 2020",
+                        destination_city: "Madison, WI",
+                        mileage: 1100)
+
+      @trip2 = Trip.create(title: "Who is America Anyway?",
+                        destination_city: "Washington, D.C.",
+                        mileage: 300)
+
+      @trip3 = Trip.create(title: "The Big Apple",
+                        destination_city: "New York City, NY",
+                        mileage: 850)
+
+      @trip4 = Trip.create(title: "Bike n’ Climb",
+                        destination_city: "Moab, UT",
+                        mileage: 700)
+    end
+
+    describe "class methods" do
+      it ".all_by_mileage" do
+        expect(Trip.all_by_mileage).to match_array([@trip2, @trip4, @trip3, @trip1])
+      end
+    end
+  end
 end
