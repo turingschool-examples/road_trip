65aa8b451a875e3a65b298a8697af96c951890cd
Margo Flewelling <flewelling.margo@gmail.com>
2020-04-20 10:38:00 -0600

    Write method and test for finding similar trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,8 @@ class Trip < ApplicationRecord
   has_many :traveler_trips
   has_many :travelers, through: :traveler_trips
 
+  def similar_trips
+    Trip.where(destination_city: self.destination_city).where.not(id: self.id)
+  end
+
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Trip, type: :model do
     it {should have_many :traveler_trips}
     it {should have_many :travelers}
   end
+
+  describe "instance methods" do
+    it "similar_trips" do
+      trip1 = Trip.create(title: "Cheese Tour 2020", destination_city: "Madison, WI", mileage: 1100)
+      trip2 = Trip.create(title: "Who is America Anyway?", destination_city: "Washington, D.C", mileage: 300)
+      trip3 = Trip.create(title: "The Big Apple", destination_city: "New York City, NY", mileage: 850)
+      trip4 = Trip.create(title: "Bike n' Climb", destination_city: "Moab UT", mileage: 700)
+      trip5 = Trip.create(title: "Tour de Canyons", destination_city: "Moab UT", mileage: 400)
+      trip6 = Trip.create(title: "Cacti Cruise", destination_city: "Moab UT", mileage: 500)
+
+      expect(trip6.similar_trips).to eq([trip4, trip5])
+      expect(trip2.similar_trips).to eq([])
+
+    end
+  end
+
 end
