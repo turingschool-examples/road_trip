2fb43b0855a41adf0fb73bc8f045b64ef9d8975a
Steven Meyers <sjjmeyers@gmail.com>
2020-04-20 09:59:23 -0600

    Add trip show page route and controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/trips', to: 'trips#index'
+  get '/trips/:id', to: 'trips#show'
 end
--- spec/features/trips/index_spec.rb
+++ spec/features/trips/index_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe "As a visitor when I visit the trip index page" do
     expect(page.all('li')[2]).to have_content(trip3.title)
     expect(page.all('li')[3]).to have_content(trip1.title)
   end
+  
+  it 'I can click on the title of a trip and be taken to a trips show page' do
+    trip1 = Trip.create!( title: "Cheese Tour 2020", destination: "Madison, WI", mileage: 1100)
+    visit '/trips'
+    click_on trip1.title
+    expect(current_path).to eq("/trips/#{trip1.id}")
+  end
 end
