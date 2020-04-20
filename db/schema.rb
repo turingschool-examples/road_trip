a216ab2ef62ea9453b51e2fdfe7092d0de206dae
Javier Aguilar <jvr.agx@gmail.com>
2020-04-20 09:06:51 -0600

    Add traveler table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200420150406) do
+ActiveRecord::Schema.define(version: 20200420150620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "travelers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trips", force: :cascade do |t|
     t.string "title"
