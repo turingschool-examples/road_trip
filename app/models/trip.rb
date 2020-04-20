18b32df62e821f603ea1433f15b1b66dcc65b98a
Steven Meyers <sjjmeyers@gmail.com>
2020-04-20 09:51:59 -0600

    Add sort by mileage class method

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,8 @@
 class Trip < ApplicationRecord
   has_many :traveler_trips
   has_many :travelers, through: :traveler_trips
+
+  def self.sort_by_mileage
+    order(:mileage)    
+  end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -6,4 +6,16 @@ describe Trip, type: :model do
     it { should have_many :traveler_trips}
     it { should have_many(:travelers).through(:traveler_trips) }
   end
+
+  describe 'class methods' do
+    it '#sort_by_mileage' do
+    trip1 = Trip.create!( title: "Cheese Tour 2020", destination: "Madison, WI", mileage: 1100)
+    trip2 = Trip.create!( title: "Who is America Anyway?", destination: "Washington, D.C.", mileage: 300)
+    trip3 = Trip.create!( title: "The Big Apple", destination: "New York City, NY", mileage: 850)
+    trip4 = Trip.create!( title: "Bike and Climb", destination: "Moab, UT", mileage: 700)
+
+
+    expect(Trip.sort_by_mileage).to eq([trip2, trip4, trip3, trip1])
+    end
+  end
 end
