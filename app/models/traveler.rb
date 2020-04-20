94225eb1dcbaa25799fe8c8d8589c683c0cd736e
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-20 11:00:51 -0600

    Add validations

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -1,5 +1,6 @@
 class Traveler < ApplicationRecord
+  validates_presence_of :name, :age
+
   has_many :trip_travelers
   has_many :trips, through: :trip_travelers
-  
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,8 @@
 class Trip < ApplicationRecord
+  validates_presence_of :destination, :title, :mileage
+
   has_many :trip_travelers
   has_many :travelers, through: :trip_travelers
-
 
   def similar_trips
     Trip.where(destination: self.destination).where.not(id: self.id)
