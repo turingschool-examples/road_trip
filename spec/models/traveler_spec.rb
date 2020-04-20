c2beda65972e06f9dad5b5714a7eeb9e55025c4c
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-20 11:33:09 -0600

    Add validation tests to model tests

--- spec/models/traveler_spec.rb
+++ spec/models/traveler_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Traveler, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :age }
+
+  end
   describe 'relationships' do
     it {should have_many :traveler_trips}
     it {should have_many(:trips).through(:traveler_trips)}
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :destination_city }
+    it { should validate_presence_of :mileage }
+
+  end
+
   describe 'relationships' do
     it {should have_many :traveler_trips}
     it {should have_many(:travelers).through(:traveler_trips)}
