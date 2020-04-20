55d996891279fa048d17e2166f2daad3f01dee36
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-20 11:20:52 -0600

    Create test for similar trips

---
+++ spec/features/similar_trips_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "similar trips" do
+
+  before(:each) do
+    @trip_1 = Trip.create(title: "Cheese Tour 2020", destination_city: "Madison, WI", mileage: 1100)
+    @trip_2 = Trip.create(title: "Who is America Anyway?", destination_city: "Washington, D.C", mileage: 300)
+    @trip_3 = Trip.create(title: "The Big Apple", destination_city: "New York City, NY", mileage: 850)
+    @trip_4 = Trip.create(title: "Bike n’ Climb", destination_city: "Moab, UT", mileage: 700)
+    @trip_5 = Trip.create(title: "The land of over 14,000 lakes", destination_city: "Madison, WI", mileage: 450)
+    @trip_6 = Trip.create(title: "A Winter Wonderland For Trout", destination_city: "Madison, WI", mileage: 175)
+    @trip_7 = Trip.create(title: "Snowmobile Heaven!", destination_city: "Madison, WI", mileage: 200)
+  end
+
+  describe "I see a section of the page titled 'Other trips to this destination'" do
+    it "in this section I see all other trip titles that have the same destination as this trip" do
+
+      visit "/trips/#{@trip_1.id}"
+
+      expect(page).to have_content("Other Trips to this Destination")
+
+      within ".other-trips" do
+        expect(page).to have_content("#{@trip_5.title}")
+        expect(page).to have_content("#{@trip_6.title}")
+        expect(page).to have_content("#{@trip_7.title}")
+        expect(page).to_not have_content("#{@trip_1.title}")
+        expect(page).to_not have_content("#{@trip_2.title}")
+        expect(page).to_not have_content("#{@trip_3.title}")
+      end
+    end
+
+    it "other trips to this destination should not include the current trip I am on" do
+    end
+
+    it "all the names of the similar trips are links to that trips show page"
+end
