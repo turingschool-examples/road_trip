835741a597094dd863f7ff7567fe4d9e139e479e
Kevin McGrevey <niv3kmcg@gmail.com>
2020-04-20 11:57:45 -0600

    Cleanup commnets

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources :trips
-  # namespace :travelers do
-  #   resources :trips
-  # end
+ 
   get '/trips/:id', to: 'trips#show'
-
   get '/traveler/:id/trips', to: 'trips#index'
-  # get 'traveler/:id/trips/:trip_id', to: 'trips#show'
   delete '/traveler/:id/trips/:trip_id', to: 'trips#remove_traveler'
 end
