1ed461ec2389a1e3741d9fb79ef517b72bd96a11
Colin Alexander <colintalex@gmail.com>
2020-04-20 11:30:15 -0600

    Adds SimilarTrips method to Trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,9 @@
 class Trip < ApplicationRecord
   has_many :traveler_trips
   has_many :travelers, through: :traveler_trips
+
+  def similar_trips
+    trips = Trip.where(destination: self.destination)
+    trips.where.not(id:self.id)
+  end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -4,4 +4,19 @@ RSpec.describe Trip, type: :model do
   describe "relationships" do
     it { should have_many(:travelers).through(:traveler_trips)}
   end
+
+  before(:each) do
+    @traveler_1 = Traveler.create(name: 'Sally Sue', age: 25)
+    @trip_1 = @traveler_1.trips.create(title: "Cheese Tour 2020", destination: "Madison, WI", mileage: 900)
+    @trip_2 = @traveler_1.trips.create(title: "Walk America", destination: "Santa Fe, NM", mileage: 5100)
+    @trip_3 = @traveler_1.trips.create(title: "National Bar Crawl", destination: "Madison, WI", mileage: 1100)
+    @trip_4 = @traveler_1.trips.create(title: "Scooter Tour", destination: "Green Bay, WI", mileage: 40)
+
+    @traveler_2 = @trip_1.travelers.create(name: 'Dapper Dan', age: 22)
+    @traveler_3 = @trip_1.travelers.create(name: 'Timmy Tom', age: 29)
+  end
+
+  describe 'Instance Methods' do
+    expect(@trip_1.similar_trips).to eql(@trip_3)
+  end
 end
