cf7d50aeb3d22e4320c8b62eb9c0bce309e4b305
Steven Meyers <sjjmeyers@gmail.com>
2020-04-20 11:48:25 -0600

    Add other trips to same destination on trip show page

--- spec/features/trips/show_spec.rb
+++ spec/features/trips/show_spec.rb
@@ -57,13 +57,29 @@ RSpec.describe "As a visitor when I visit the trip show" do
     TravelerTrip.create!(trip: trip1, traveler: traveler2)
 
     visit "/trips/#{trip1.id}"
-    
+
     within ("#traveler-#{traveler1.id}") do
       click_button 'Remove Traveler'
     end 
-    expect(current_path).to eq("/trips/#{trip1.id}")
 
+    expect(current_path).to eq("/trips/#{trip1.id}")
     expect(page).to_not have_content("Sally Sue")
   end
 
+  it 'I see a section called Other Trips to this Destination with a list of trips that are also to this destination' do
+    trip1 = Trip.create!( title: "Cheese Tour 2020", destination: "Madison, WI", mileage: 1100)
+    trip2 = Trip.create!( title: "Boats and Stuff", destination: "Madison, WI", mileage: 1000)
+    trip3 = Trip.create!( title: "Giant Capital Building", destination: "Madison, WI", mileage: 900)
+    trip4 = Trip.create!( title: "Where A Lake Used to Be", destination: "Death Valley, CA", mileage: 1100)
+  
+    visit "/trips/#{trip1.id}"
+
+    within(".other-trips") do 
+      expect(page).to have_content("Other Trips to this Destination:")
+      expect(page).to have_content(trip2.title)
+      expect(page).to have_content(trip3.title)
+      expect(page).to_not have_content(trip1.title)
+      expect(page).to_not have_content(trip4.title)
+    end 
+  end
 end
