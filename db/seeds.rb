30dd52259b6e95890f675a6129e9d3c066b4a9a1
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-20 10:26:18 -0600

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-traveler1 = Traveler.create(name: “Sally Sue”, age: 25)
-traveler2 = Traveler.create(name: “John Wick”, age: 20)
-traveler3 = Traveler.create(name: “Luke Skywalker”, age: 80)
-traveler4 = Traveler.create(name: “Graham Cracker”, age: 32)
+traveler1 = Traveler.create(name: "Sally Sue", age: 25)
+traveler2 = Traveler.create(name: "John Wick", age: 20)
+traveler3 = Traveler.create(name: "Luke Skywalker", age: 80)
+traveler4 = Traveler.create(name: "Graham Cracker", age: 32)
 
 
-trip1 = Trip.create(name: “Cheese Tour 2020”, destination_city: “Madison, WI”, mileage: 1100)
-trip2 = Trip.create(name: “Who is America Anyway?”, destination_city: “Washington, D.C.”, mileage: 300)
-trip3 = Trip.create(name: “The Big Apple”, destination_city: “New York City, NY”, mileage: 850)
-trip4 = Trip.create(name: “Bike n’ Climb”, destination_city: “Moab, UT”, mileage: 700)
+trip1 = Trip.create(name: "Cheese Tour 2020", destination_city: "Madison, WI", mileage: 1100)
+trip2 = Trip.create(name: "Who is America Anyway?", destination_city: "Washington, D.C.", mileage: 300)
+trip3 = Trip.create(name: "The Big Apple", destination_city: "New York City, NY", mileage: 850)
+trip4 = Trip.create(name: "Bike n’ Climb", destination_city: "Moab, UT", mileage: 700)
 
 TravelerTrip.create()
