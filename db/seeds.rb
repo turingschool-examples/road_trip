578811d68afc7cab152bee7b3277b0523c9712ff
David Tran <davidttemail@gmail.com>
2020-04-20 09:43:16 -0600

    Create travelers and traveler_trips table

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,28 +9,21 @@
 trip_1 = Trip.create(title: "Cheese Tour 2020", destination_city: "Madison, WI", mileage: 1100)
 trip_2 = Trip.create(title: "Who is America Anyway?", destination_city: "Washington, D.C.", mileage: 300)
 trip_3 = Trip.create(title: "The Big Apple", destination_city: "New York City, NY", mileage: 850)
+trip_4 = Trip.create(title: "Bike n’ Climb", destination_city: "Moab, UT", mileage: 700)
 
-# Trip4
-# title: "Bike n’ Climb"
-# destination_city: "Moab, UT"
-# mileage: 700
-#
-# Traveler1
-# name: "Sally Sue"
-# age: 25
-#
-# Traveler2
-# name: "Tommy Tom"
-# age: 46
-#
-# Traveler3
-# name: "Cory Cory"
-# age: 19
-#
-# Traveler4
-# name: "Mary Mae"
-# age: 44
-#
-# Traveler5
-# name: "Smith John"
-# age: 9
+traveler_1 = Traveler.create(name: "Sally Sue", age: 25)
+traveler_2 = Traveler.create(name: "Tommy Tom", age: 46)
+traveler_3 = Traveler.create(name: "Cory Cory", age: 19)
+traveler_4 = Traveler.create(name: "Mary Mae", age: 44)
+traveler_5 = Traveler.create(name: "Smith John", age: 9)
+
+traveler_trip_1 = TravelerTrip.create(trip: trip_1, traveler: traveler_1)
+traveler_trip_2 = TravelerTrip.create(trip: trip_1, traveler: traveler_2)
+traveler_trip_3 = TravelerTrip.create(trip: trip_2, traveler: traveler_2)
+traveler_trip_4 = TravelerTrip.create(trip: trip_3, traveler: traveler_5)
+traveler_trip_5 = TravelerTrip.create(trip: trip_4, traveler: traveler_2)
+traveler_trip_6 = TravelerTrip.create(trip: trip_4, traveler: traveler_3)
+traveler_trip_7 = TravelerTrip.create(trip: trip_5, traveler: traveler_1)
+traveler_trip_8 = TravelerTrip.create(trip: trip_5, traveler: traveler_2)
+traveler_trip_9 = TravelerTrip.create(trip: trip_5, traveler: traveler_3)
+traveler_trip_10 = TravelerTrip.create(trip: trip_5, traveler: traveler_4)
