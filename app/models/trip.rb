d0bd46e5133b6a8cc3da9e1d30db08d8fbe6553c
Stephanie Friend <stephaniefriend91@gmail.com>
2020-04-20 11:58:06 -0600

    Transfered in-code activerecord to model with coorisponding test

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,8 @@ class Trip < ApplicationRecord
   has_many :travelers, through: :traveler_trips
 
   validates_presence_of :title, :destination_city, :mileage
+
+  def self.trip_order
+    order(:mileage)
+  end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe Trip, type: :model do
     it {should have_many(:travelers).through(:traveler_trips)}
 
   end
+
+  describe 'class methods' do
+    it 'trip order' do
+    trip1 = Trip.create(title: "Cheese Tour 2020",
+                        destination_city: "Madison WI",
+                        mileage: 1100)
+
+    trip2 = Trip.create(title: "Who is America Anyway?",
+                        destination_city: "Washington, D.C.",
+                        mileage: 300)
+
+    trip3 = Trip.create(title: "The Big Apple",
+                        destination_city: "New York City, NY",
+                        mileage: 850)
+
+    expect(Trip.trip_order).to eq([trip2, trip3, trip1])
+    end
+  end
 end
