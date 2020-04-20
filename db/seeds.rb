5f9389e61a406c3a79e9b3c458285883c36ea3f6
“Krista <stadler.krista@gmail..com>
2020-04-20 10:43:10 -0600

    Finished User Story 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-@traveler_2 = Traveler.create(name: "Tommy Tom", age: 46)
-@traveler_3 = Traveler.create(name: "Cory Cory", age: 19)
-@traveler_4 = Traveler.create(name: "Mary Mae", age: 44)
-@traveler_5 = Traveler.create(name: "Smith John", age: 9)
+@traveler_1 = Traveler.create(name: "Tommy Tom", age: 46)
+@traveler_2 = Traveler.create(name: "Cory Cory", age: 19)
+@traveler_3 = Traveler.create(name: "Mary Mae", age: 44)
+@traveler_4 = Traveler.create(name: "Smith John", age: 9)
 
 @trip_1 = Trip.create!(title: "Cheese Tour 2020", destination: "Madison, WI", mileage: 1100)
 @trip_2 = Trip.create!(title: "Who is America Anyway?", destination: "Washington, D.C.", mileage: 300)
 @trip_3 = Trip.create!(title: "The Big Apple", destination: "New York City, NY", mileage: 850)
 @trip_4 = Trip.create!(title: "Bike n’ Climb", destination: "Moab, UT", mileage: 700)
+
+TravelerTrip.create!(trip: @trip_1, traveler: @traveler_1)
+TravelerTrip.create!(trip: @trip_1, traveler: @traveler_4)
---
+++ spec/features/trips/show_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "as a visitor" do
+  describe "When I visit a trips show page" do
+    before(:each) do
+
+      @trip_1 = Trip.create(title: "Cheese Tour 2020", destination: "Madison, WI", mileage: 1100)
+      @trip_2 = Trip.create(title: "Who is America Anyway?", destination: "Washington, D.C.", mileage: 300)
+      @trip_3 = Trip.create(title: "The Big Apple", destination: "New York City, NY", mileage: 850)
+      @trip_4 = Trip.create(title: "Bike n’ Climb", destination: "Moab, UT", mileage: 700)
+
+      @traveler_1 = Traveler.create(name: "Tommy Tom", age: 46)
+      @traveler_2 = Traveler.create(name: "Cory Cory", age: 19)
+      @traveler_3 = Traveler.create(name: "Mary Mae", age: 44)
+      @traveler_4 = Traveler.create(name: "Smith John", age: 9)
+
+      TravelerTrip.create!(trip: @trip_1, traveler: @traveler_1)
+      TravelerTrip.create!(trip: @trip_1, traveler: @traveler_4)
+
+    end
+
+    it "the trip show page shows the trips title, destination city, mileage, and travelers" do
+      visit "/trips/#{@trip_1.id}"
+
+      expect(page).to have_content(@trip_1.title)
+      expect(page).to have_content(@trip_1.destination)
+      expect(page).to have_content(@trip_1.mileage)
+      expect(page).to have_content(@traveler_1.name)
+      expect(page).to have_content(@traveler_4.name)
+
+      expect(page).to_not have_content(@trip_2.title)
+      expect(page).to_not have_content(@trip_3.title)
+      expect(page).to_not have_content(@trip_4.title)
+      expect(page).to_not have_content(@traveler_3.name)
+      expect(page).to_not have_content(@traveler_2.name)
+
+    end
+  end
+end
