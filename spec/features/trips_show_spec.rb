839806358fedcb415622e9208bc80f1269128f5a
Mike Hernandez <hernandezmichael321@gmail.com>
2020-04-20 09:50:47 -0600

    Create traveler model and update DB

--- spec/features/trips_show_spec.rb
+++ spec/features/trips_show_spec.rb
@@ -6,6 +6,27 @@ RSpec.describe "when I visit the trip show page and click the name of a trip" do
     @trip_2 = Trip.create(title: "Who is America Anyway?", destination_city: "Washington, D.C", mileage: 300)
     @trip_3 = Trip.create(title: "The Big Apple", destination_city: "New York City, NY", mileage: 850)
     @trip_4 = Trip.create(title: "Bike n’ Climb", destination_city: "Moab, UT", mileage: 700)
+
+#     Traveler1
+# name: “Sally Sue”
+# age: 25
+#
+# Traveler2
+# name: “Tommy Tom”
+# age: 46
+#
+# Traveler3
+# name: “Cory Cory”
+# age: 19
+#
+# Traveler4
+# name: “Mary Mae”
+# age: 44
+#
+# Traveler5
+# name: “Smith John”
+# age: 9
+
   end
 
   it "I am taken to the trips show page" do
