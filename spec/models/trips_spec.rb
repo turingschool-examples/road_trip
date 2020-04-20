7f9429370e7089d63560d52c5a67d56889f03584
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-20 09:34:07 -0600

    Create relationships at model level

--- spec/models/trips_spec.rb
+++ spec/models/trips_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Trip do
   describe "relationships" do
     it { should have_many(:traveler_trips) }
-     it { should have_many(:trips).through(:traveler_trips) } 
+     it { should have_many(:travelers).through(:traveler_trips) } 
   end
 end
