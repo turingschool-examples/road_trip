b78427854c6a2ed0978d85800cb483f5b31e5516
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-04-20 10:54:33 -0600

    Add similar_trips method to Trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,8 @@ class Trip < ApplicationRecord
 
   has_many :traveler_trips
   has_many :travelers, through: :traveler_trips
+
+  def similar_trips
+    Trip.where("city = ? AND id != ?", self.city, self.id)
+  end
 end
