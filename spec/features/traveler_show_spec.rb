6cbdc2cd24e1f4399806b22021ec9bbb84f86653
Ana Perez Santos <a.jperez@yahoo.com>
2020-04-21 09:36:00 -0600

    US 1: create traveler show feature spec/route/controller/action/view

---
+++ spec/features/traveler_show_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "Traveler Show Page" do
+  describe "when I visit the /travelers/:id (show page)" do
+    it "I see the travelers name and age, along with trip information" do
+      trip1 = Trip.create!(title: "Cheese Tour 2020", destination: "Madison, WI", mileage: 1100)
+      trip2 = Trip.create!(title: "Who is America Anyway?", destination: "Washington, D.C.", mileage: 300)
+
+      traveler = Traveler.create!(name: "Sally Sue", age: 25)
+
+      TravelerTrip.create!(trip_id: trip1.id, traveler_id: traveler.id)
+      TravelerTrip.create!(trip_id: trip2.id, traveler_id: traveler.id)
+
+      visit "/travelers/#{traveler.id}"
+
+      expect(page).to have_content(traveler.name)
+      expect(page).to have_content(traveler.age)
+      expect(page).to have_content(trip1.title)
+      expect(page).to have_content(trip2.title)
+      expect(page).to have_content("Average Trip Mileage: 700")
+    end
+  end
+end
