89cfadfc9a7de22b4fa0ffba820c4c94d87562b2
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-21 11:00:33 -0600

    add /travelers/:id/trips/new to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # travelers
   get '/travelers/:id', to: 'travelers#show'
+  # traveler_trips
+  post '/travelers/:id/trips/new', to: 'traveler_trips#create'
 end
